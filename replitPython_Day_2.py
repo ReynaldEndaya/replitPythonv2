22fbdef65e9c70450a1746fb64b5d5c664a48953
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-06-13 16:22:07 +0800

    2nd Day of Python

---
+++ replitPython_Day_2.py
@@ -0,0 +1,5 @@
+faveFood = input('What is your favorite food? ')
+faveMusic = input('What is your favorite music? ')
+address = input('Where do you live? ')
+
+print('Hello! Your favorite food is',faveFood,'your favorite music is ',faveMusic,'and you live in',address)
