61a365d02a9324ce9a677bbe4d8a28b684dbe92d
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-06-16 14:34:52 +0800

    day 3 of replit 100 days of code

---
+++ replitPython_Day_3.py
@@ -0,0 +1,10 @@
+#Simple program to create a made up recipe using concatenate function in Python
+
+food = input('Name a food > ')
+plant = input('Name a plant > ')
+cookingMethod = input('Name a method of cooking > ')
+burnedFoodDescription = input('What word describes burned food > ')
+DIYItem = input('Name a DIY Item >')
+
+print('Menu')
+print(cookingMethod, food, 'with', burnedFoodDescription, plant, 'on a bed of', DIYItem)
