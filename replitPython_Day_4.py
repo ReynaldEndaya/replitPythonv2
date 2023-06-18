4fa672329e085f9a6c4c762474e4521fed94a4fb
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-06-18 15:53:37 +0800

    added all the variables

--- replitPython_Day_4.py
+++ replitPython_Day_4.py
@@ -1,1 +1,9 @@
 print("""Welcome to our adventure simulator! I am going to ask you a bunch of questions and then create an epic story with you as the star!""")
+
+name = input('What is your name? ')
+favoriteFood = input('What is your favorite food? ')
+superpower = input('What is your superpower? ')
+address = input('Where do you live? ')
+worstEnemy = input('What is your worst enemy\'s name? ')
+
+print('Hello',"\033[32m",name,"\033[0m",'today, master Oogway has chosen you to become the new Dragon Warrior!','You will be  ')
