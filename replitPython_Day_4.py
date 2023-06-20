a95548887ebaca3fe72f5225faf133ff61fc1546
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-06-20 13:35:02 +0800

    final for day 4

--- replitPython_Day_4.py
+++ replitPython_Day_4.py
@@ -6,4 +6,9 @@ superpower = input('What is your superpower? ')
 address = input('Where do you live? ')
 worstEnemy = input('What is your worst enemy\'s name? ')
 
-print('Hello',"\033[32m",name,"\033[0m",'today, master Oogway has chosen you to become the new Dragon Warrior!','You will be  ')
+print('Hello',"\033[32m",name,"\033[0m",'today, master Oogway has chosen you to become the new Dragon Warrior!',
+      'You will be trained in the advanced skill of martial arts,self-discipline, and perseverance!','Forget about your',
+      "\033[36m",favoriteFood,"\033[0m",'there is no place here for such weakness!,Aside from Kung Fu we will also develop your',
+      "\033[33m",superpower,"\033[0m",'skills and this will be your secret weapon when things get too tight','You will move out of your current home in',
+      "\033[32m",address,"\033[0m",'and you will stay in our fortress for the training','here there be monsters and a lot of your worst enemy --',
+      "\033[31m",worstEnemy,"\033[0m")
