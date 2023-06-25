9b69a40fd9c977ec3d695c5582be9f2252e2f678
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-06-26 00:18:20 +0800

    final day 5 output

--- replitPython_Day_5.py
+++ replitPython_Day_5.py
@@ -1,3 +1,23 @@
 print('WanPanMan Character Generator')
+print('Remember, answer with yes or no only!')
 
+character = input('Can you defeat your enemies with one punch? ')
 
+if character == 'yes':
+    print('Hello Saitama!')
+elif character == 'no':
+    character = input('Are you an overpowered cyborg but is always turned into scrap? ')
+    if character == 'yes':
+        print('Hello Genos!')
+    elif character == 'no':
+        character = input('Are you known for your "engine"? ')
+        if character == 'yes':
+            print('Hello King!')
+        elif character == 'no':
+            print('I don\'t know what you are but I sure hope I get to know you!')
+        else:
+            print('Wrong Input friend!')
+    else:
+        print('Wrong Input friend!')
+else:
+    print('Wrong Input friend!')
