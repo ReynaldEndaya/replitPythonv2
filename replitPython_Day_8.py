9dc5d299f8627492e4e1bcd815474b0b92b224bb
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-06-28 19:30:52 +0800

    day 8 first commit

---
+++ replitPython_Day_8.py
@@ -0,0 +1,10 @@
+print('Affirmation Generator!')
+
+name = input('What is your name? ')
+currentDay = input('What is it today? ')
+faveFood = input('What is your favorite food? ')
+faveSports = input('What sports do you usually play? ')
+faveGame = input('What video game are you playing right now? ')
+faveBook = input('What is your favorite book? ')
+faveAnime = input('What anime are you currently watching? ')
+fave
