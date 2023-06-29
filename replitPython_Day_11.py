7dacb50b7193afe73d3cb8348b74fd38006983c5
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-06-29 19:13:38 +0800

    day 11 final

---
+++ replitPython_Day_11.py
@@ -0,0 +1,10 @@
+print('This program will compute how many seconds in a year!')
+
+year = int(input('What year do you want to compute for? '))
+
+if year % 4 == 0:
+    seconds = 366 * 24 * 60 * 60
+    print('There are',seconds,'seconds in',year)
+else:
+    seconds = 365 * 24 * 60 * 60
+    print('There are',seconds,'seconds in',year)
