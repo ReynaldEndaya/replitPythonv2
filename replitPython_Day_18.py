79dbeca701055d9c5aedbb1a63d22d68e4bd2ed8
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-07-03 03:35:09 +0800

    day 18 final

---
+++ replitPython_Day_18.py
@@ -0,0 +1,21 @@
+print('This is a numbers guessing game!')
+print()
+
+number = 4000
+attempts = 0
+
+while True:
+
+    numberGuess = int(input('What is your guess? '))
+    attempts += 1
+    if numberGuess > number:
+        print('Your guess is too high. Guess again')
+    elif numberGuess < number and numberGuess >=0:
+        print('Your guess is too low. Guess again')
+    elif numberGuess == number:
+        print('That is correct! You only took',attempts,'guesses')
+        break
+    else:
+        print('Invalid number')
+        
+    
