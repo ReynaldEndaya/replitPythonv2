d20ed1fac4381432e63ecdefc7697db27913ef55
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-07-03 03:03:49 +0800

    day 17 final commit

--- replitPython_Day_17.py
+++ replitPython_Day_17.py
@@ -8,39 +8,48 @@ playerTwoScore = 0
 
 while True:
     
-    playerOne = input('Player 1: (R)ock, (P)aper, or (S)cissor: ')
-    playerTwo = input('Player 2: (R)ock, (P)aper, or (S)cissor: ')
+    if playerOneScore < 3 and playerTwoScore < 3:
+        
+        playerOne = input('Player 1: (R)ock, (P)aper, or (S)cissor: ')
+        playerTwo = input('Player 2: (R)ock, (P)aper, or (S)cissor: ')
 
-    if playerOne.lower() == playerTwo.lower(): #Script for tied outcome
-        if playerOne.lower() == 's':
-            print('You both chose Scissor. It\'s a tie!')
-        elif playerOne.lower() == 'r':
-            print('You both chose Rock. It\'s a tie!')
+        if playerOne.lower() == playerTwo.lower(): #Script for tied outcome
+            if playerOne.lower() == 's':
+                print('You both chose Scissor. It\'s a tie!')
+            elif playerOne.lower() == 'r':
+                print('You both chose Rock. It\'s a tie!')
+            else:
+                print('You both chose Paper. It\'s a tie!')
+        
+        #Script for Player 1 winning
+        elif playerOne.lower() == 'r' and playerTwo.lower() == 's':
+            print('Player 1 chose Rock and Player 2 chose scissor. Player 1 Wins!')
+            playerOneScore += 1
+        elif playerOne.lower() == 's' and playerTwo.lower() == 'p':
+            print('Player 1 chose Scissor and Player 2 chose Paper. Player 1 Wins!')
+            playerOneScore += 1
+        elif playerOne.lower() == 'p' and playerTwo.lower() == 'r':
+            print('Player 1 chose Paper and Player 2 chose Rock. Player 1 Wins!')
+            playerOneScore += 1
+        
+        #Script for player 2 winning
+        elif playerTwo.lower() == 'r' and playerOne.lower() == 's':
+            print('Player 1 chose Scissor and Player 2 chose Rock. Player 2 Wins!')
+            playerTwoScore += 1
+        elif playerTwo.lower() == 's' and playerOne.lower() == 'p':
+            print('Player 1 chose Paper and Player 2 chose Scissor. Player 2 Wins!')
+            playerTwoScore += 1
+        elif playerTwo.lower() == 'p' and playerOne.lower() == 'r':
+            print('Player 1 chose Rock and Player 2 chose Paper. Player 2 Wins!')
+            playerTwoScore += 1
         else:
-            print('You both chose Paper. It\'s a tie!')
-    
-    #Script for Player 1 winning
-    elif playerOne.lower() == 'r' and playerTwo.lower() == 's':
-        print('Player 1 chose Rock and Player 2 chose scissor. Player 1 Wins!')
-        playerOneScore += 1
-    elif playerOne.lower() == 's' and playerTwo.lower() == 'p':
-        print('Player 1 chose Scissor and Player 2 chose Paper. Player 1 Wins!')
-        playerOneScore += 1
-    elif playerOne.lower() == 'p' and playerTwo.lower() == 'r':
-        print('Player 1 chose Paper and Player 2 chose Rock. Player 1 Wins!')
-        playerOneScore += 1
-    
-    #Script for player 2 winning
-    elif playerTwo.lower() == 'r' and playerOne.lower() == 's':
-        print('Player 1 chose Scissor and Player 2 chose Rock. Player 2 Wins!')
-        playerTwoScore += 1
-    elif playerTwo.lower() == 's' and playerOne.lower() == 'p':
-        print('Player 1 chose Paper and Player 2 chose Scissor. Player 2 Wins!')
-        playerTwoScore += 1
-    elif playerTwo.lower() == 'p' and playerOne.lower() == 'r':
-        print('Player 1 chose Rock and Player 2 chose Paper. Player 2 Wins!')
-        playerTwoScore += 1
+            print('Invalid Choice')
     
     else:
-        print('Invalid Choice')
-    
+        if playerOneScore > playerTwoScore:
+            print('Player 1 with',playerOneScore,'points and player 2 with',playerTwoScore,'points player 1 wins')
+        else:
+            print('Player 2 with',playerTwoScore,'points and player 1 with',playerOneScore,'points player 2 wins')
+        break
+        
+        
