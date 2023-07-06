96d7397951b79f87132b2574a750b82b0b10b491
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-07-06 20:51:35 +0800

    day 21 final commit

--- replitPython_Day_21.py
+++ replitPython_Day_21.py
@@ -1,1 +1,21 @@
 print('Welcome to Math Facts!')
+print()
+
+number = int(input('Name your multiples: '))
+score = 0
+
+for counter in range(1,11):
+    print(counter,' x ',number,'= ',end='')
+    answer = int(input())
+    if answer == counter * number:
+        print('Great work! That\'s correct!')
+        print()
+        score += 1
+        continue
+    else:
+        print('Sorry that\'s wrong. Correct answer is',counter*number)
+        print()
+
+print('You got ',score,' out of 10')
+print()
+    
