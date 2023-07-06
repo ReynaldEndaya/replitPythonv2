4862b70072dfe9f15ca666c52b67b3eda8102a39
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-07-06 21:43:55 +0800

    day 23 final commit

---
+++ replitPython_Day_23.py
@@ -0,0 +1,19 @@
+print('Strix Login System')
+
+def login():
+    
+    while True:
+
+        username = input('Username: ')
+        password = input('Password: ')
+            
+        if username.lower() == 'reynald' and password == 'reynald3':
+            print('Welcome Reynald')
+            break
+        else:
+            print('Invalid Credentials')
+            continue
+
+
+login()
+    
