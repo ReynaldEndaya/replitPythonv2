d85ece2cdf8953b5336ae20447164991aba01ffc
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-07-15 12:29:56 +0800

    day 25 first commit

---
+++ replitPython_Day_25.py
@@ -0,0 +1,36 @@
+# Let's extend Day 24's project and create a health stats generator for a character in a video game.
+
+# 1. Create a subroutine that rolls a dice of any size and returns that number.
+# 2. Then, create a second subroutine that rolls one six-sided dice and one eight-sided dice.
+# 3. Multiply the number from the six-sided dice and eight-sided dice together and return that subroutine as a character's health stats for a video game.
+# 4. Print out the character's health stats.
+# 5. Add a loop to give the user the option to generate health stats for another character.
+
+
+import random
+
+print('Roll a Dice')
+
+def rollDice(sides):
+    
+    # while True:    
+    number = random.randint(1,sides)
+        # print('You rolled ',number)
+        # repeat = input('Roll again? (y|n): ')
+        # if repeat.lower() == 'n':
+        #     break
+        # else:
+        #     continue
+    
+    return number
+
+def healthStatsCreator():
+    health = rollDice(6) * rollDice(8)
+    return health
+    
+while True:
+    
+    warriorName = input('Name your warrior: ')
+    warriorHealth = healthStatsCreator()
+    print('Their health is',warriorHealth)
+    
