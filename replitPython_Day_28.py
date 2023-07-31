92e2c5ee18be5a4b3a6b9f72f5cf250c7cc4285f
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-07-31 14:24:15 +0800

    day 28 final commit'

--- replitPython_Day_28.py
+++ replitPython_Day_28.py
@@ -26,40 +26,44 @@ def characterProfile():
     characterType = input('Character Type (Human, Elf, Wizard, Orc): ')
     return characterName
 
+os.system('clear')
+print('⚔️ Battle Time ⚔️')
+
+print()
+characterName_1 = characterProfile()
+health_1 = healthStatsCreator()
+strength_1 = strengthStatsCreator()
+print()
+
+print(characterName_1)
+time.sleep(1)
+print('Health:',health_1)
+time.sleep(1)
+print('Strength:',strength_1)
+time.sleep(1)
+print()
+print()
+time.sleep(1)
+
+print('Who are they battling?')
+
+print()
+characterName_2 = characterProfile()
+health_2 = healthStatsCreator()
+strength_2 = strengthStatsCreator()
+print()
+
+print(characterName_2)
+time.sleep(1)
+print('Health:',health_2)
+time.sleep(1)
+print('Strength:',strength_2)
+time.sleep(1)
+print()
+
+round_counter = 0
+
 while True:
-    os.system('clear')
-    print('⚔️ Battle Time ⚔️')
-    
-    print()
-    characterName_1 = characterProfile()
-    health_1 = healthStatsCreator()
-    strength_1 = strengthStatsCreator()
-    print()
-    
-    print(characterName_1)
-    time.sleep(1)
-    print('Health:',health_1)
-    time.sleep(1)
-    print('Strength:',strength_1)
-    time.sleep(1)
-    print()
-    print()
-    time.sleep(1)
-    
-    print('Who are they battling?')
-    
-    print()
-    characterName_2 = characterProfile()
-    health_2 = healthStatsCreator()
-    strength_2 = strengthStatsCreator()
-    
-    print(characterName_2)
-    time.sleep(1)
-    print('Health:',health_2)
-    time.sleep(1)
-    print('Strength:',strength_2)
-    time.sleep(1)
-    print()
     
     os.system('clear')
     print('⚔️ Battle Time ⚔️')
@@ -67,7 +71,45 @@ while True:
     print('The Battle Begins')
     print()
     
-    if 
+    round_counter += 1
+    
+    character_1_roll_dice = rollDice(6)
+    character_2_roll_dice = rollDice(6)
+    
+    damage = abs(strength_1 - strength_2) + 1
+    
+    if character_1_roll_dice > character_2_roll_dice:
+      health_2 = health_2 - damage
+      print(characterName_1,'wins round',round_counter,'!')
+      print(characterName_2,'takes a hit with',damage,'damage')
+    else:
+      health_1 = health_1 - damage
+      print(characterName_2,'wins round',round_counter,'!')
+      print(characterName_1,'takes a hit with',damage,'damage')
+      
+    time.sleep(1)
+    print()
+    print(characterName_1)
+    print('Health:',health_1)
+    time.sleep(1)
+    print()
+    print(characterName_2)
+    print('Health:',health_2)
     
     
-    
+    if health_1 > 0 and health_2 > 0:
+      time.sleep(1)
+      print()
+      print('And they\'re both standing for the next round!')
+      time.sleep(3)
+      continue
+    elif health_1 > health_2:
+      print()
+      print('Oh no',characterName_2,'has died!')
+      print(characterName_2,'wins the game in',round_counter,'rounds!')
+      break
+    else:
+      print()
+      print('Oh no',characterName_1,'has died!')
+      print(characterName_1,'wins the game in',round_counter,'rounds!')
+      break
