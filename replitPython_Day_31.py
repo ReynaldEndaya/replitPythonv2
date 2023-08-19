001572307fdced7dc38b6d22cc3a4c3bccf3cd0f
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-08-19 16:39:26 +0800

    day 31 completed interface 1

--- replitPython_Day_31.py
+++ replitPython_Day_31.py
@@ -8,14 +8,21 @@ def colorizer(color):
         return "\033[1;37m"
     elif color == 'blue':
         return "\033[0;34m"
+    elif color == 'green':
+        return "\033[0;32m"
+    elif color == 'purple':
+        return "\033[0;35m"
     else:
         return "\033[0;30m"
     
 header = f"{colorizer('red')}={colorizer('white')}={colorizer('blue')}= {colorizer('else')}Music App {colorizer('red')}={colorizer('white')}={colorizer('blue')}="
 headerLength = int(len(header))+20
-queen = f"{colorizer('yellow')}Queen"
 
 print(f"{header: ^{headerLength}}")
 print()
 print('🔥▶️',f"{'Radio Gaga': ^12}")
-print(f"{queen: ^22}")
+print(f"{colorizer('yellow')}",f"{'Queen': ^13}")
+print(colorizer('else'))
+print('PREV')
+print(f"{colorizer('green')}",f"{'NEXT': ^12}")
+print(f"{colorizer('purple')}",f"{'PAUSE': ^23}")
