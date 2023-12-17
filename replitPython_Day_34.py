c826f88a7fcbac57d138b3a4893189a73f513c34
Reynald Endaya <reynaldemmanuel.endaya@gmail.com>
2023-12-18 02:34:18 +0800

    day 34 primary function working

--- replitPython_Day_34.py
+++ replitPython_Day_34.py
@@ -11,11 +11,42 @@
 
 import os, time
 
-emailAddress = ['test1','test2']
+emailAddress = []
 
 def printList():
     for index in range(len(emailAddress)):
         print(f"{index+1} {emailAddress[index]}")
         time.sleep(1)
+
+def getEmailAddress():
+    while True:
+        os.system('clear')
+        print()
+        email = input('Enter Email Address: ')
+        emailAddress.append(email)
+        inputNewEmail = input('Input another email? (y|n): ')
+        if inputNewEmail == 'y':
+            continue
+        else:
+            break
+
+def printEmail(): 
+
+    for email in emailAddress:
+        print(f'''
+        Dear {email},
         
-printList()    
+        It has come to our attention that you\'re missing out on the amazing Replit 100 days of code. 
+        We insist you do it right away. If you don't we will pass on your email address to every spammer we've ever encountered and also sign you up to the My Little Pony newsletter, because that\'s neat. 
+        
+        We might just do that anyway.
+                
+        Love and hugs,
+        Reynald Endaya ''')
+        time.sleep(3)
+        os.system('clear')
+        time.sleep(1)
+
+getEmailAddress()
+os.system('clear')
+printEmail()
